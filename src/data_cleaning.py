772b0e05d61eb1c61867f49f0c8d58c3b7be1209
Seif EldenKhaled <seifelden368@gmail.com>
2026-08-13 20:43:31 +0300

    Update project

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,6 +1,32 @@
 import pandas as pd
 
-# 1. DATA INSPECTION
+
+
+# 1. DATA LOADING
+
+def load_data(file_path):
+    """
+    Load an Excel dataset using Pandas.
+    """
+
+    try:
+        df = pd.read_excel(file_path)
+        print("Dataset loaded successfully.")
+        return df
+
+    except FileNotFoundError:
+        print(f"File not found: {file_path}")
+        return None
+
+    except ValueError as e:
+        print(f"Invalid Excel file or format: {e}")
+        return None
+
+    except Exception as e:
+        print(f"Error while loading the dataset: {e}")
+        return None
+
+# 2. DATA INSPECTION
 
 
 def inspect_structure(df):
